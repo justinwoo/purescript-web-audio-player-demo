4d3438953eea8a42aa018ad151adf69bd6f32084
justinwoo <moomoowoo@gmail.com>
2017-02-25 10:44:02 +0200

    update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
-        './purescript-web-audio-player-demo',
-        './purescript-web-audio-player-demo/index.html',
-        './purescript-web-audio-player-demo/dist/app.js',
-        './purescript-web-audio-player-demo/dist/app.css'
+        './',
+        './index.html',
+        './dist/app.js',
+        './dist/app.css'
       ]);
     })
   );
