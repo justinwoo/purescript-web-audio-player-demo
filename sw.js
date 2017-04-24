41b14ffb3cb99c4204dc43a8fe28593c5dc1ce93
justinwoo <moomoowoo@gmail.com>
2017-04-24 16:33:09 +0200

    update and fix sw.js code

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('airhorner').then(function(cache) {
+    caches.open('halogen-web-audio-player-demo').then(function(cache) {
       return cache.addAll([
-        './',
         './index.html',
+        './index.html?homescreen=1',
         './dist/app.js',
         './dist/app.css'
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
