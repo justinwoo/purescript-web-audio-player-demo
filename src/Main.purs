abae531b3839f6ba4e42ea87ba6ad534f1ee19d3
justinwoo <moomoowoo@gmail.com>
2017-05-10 23:48:10 +0300

    some refactoring

--- src/Main.purs
+++ src/Main.purs
@@ -40,12 +40,11 @@ data Query a
   | Skip SkipDir SkipSize a
 
 type AppEffects eff =
-  Aff
   ( console :: CONSOLE
   , dom :: DOM
   | eff)
 
-ui :: forall eff. H.Component HH.HTML Query Unit Void (AppEffects eff)
+ui :: forall eff. H.Component HH.HTML Query Unit Void (Aff (AppEffects eff))
 ui =
   H.component
     { initialState: const initialState
@@ -67,17 +66,17 @@ ui =
           [ HP.class_ $ wrap "root" ]
           [ HH.h1_ [HH.text "glorious web audio thing"]
           , HH.div_
-              [ HH.input
-                [ HP.ref $ wrap "input"
-                , HP.type_ HP.InputFile
-                , HP.prop (wrap "accept")  "audio/*"
-                , HE.onChange (HE.input_ FileSet)
-                ]
+            [ HH.input
+              [ HP.ref $ wrap "input"
+              , HP.type_ HP.InputFile
+              , HP.prop (wrap "accept")  "audio/*"
+              , HE.onChange (HE.input_ FileSet)
               ]
+            ]
           , HH.div_
               [ HH.audio
                 [ HP.ref $ wrap "audio"
-                , HP.src $ unwrap $ fromMaybe (wrap "") state.file
+                , HP.src $ fromMaybe "" (unwrap <$> state.file)
                 , HP.controls true
                 , HP.autoplay true
                 ]
@@ -85,48 +84,48 @@ ui =
             ]
           , HH.div
               [ HP.class_ $ wrap "buttons" ]
-              [ HH.button [HE.onClick (HE.input_ $ Skip Bck Lg)] [HH.label_ [HH.text "<<<"]]
-              , HH.button [HE.onClick (HE.input_ $ Skip Bck Md)] [HH.label_ [HH.text "<<"]]
-              , HH.button [HE.onClick (HE.input_ $ Skip Bck Sm)] [HH.label_ [HH.text "<"]]
-              , HH.button [HE.onClick (HE.input_ $ Skip Fwd Sm)] [HH.label_ [HH.text ">"]]
-              , HH.button [HE.onClick (HE.input_ $ Skip Fwd Md)] [HH.label_ [HH.text ">>"]]
-              , HH.button [HE.onClick (HE.input_ $ Skip Fwd Lg)] [HH.label_ [HH.text ">>>"]]
+              [ HH.button [HE.onClick $ HE.input_ (Skip Bck Lg)] [HH.label_ [HH.text "<<<"]]
+              , HH.button [HE.onClick $ HE.input_ (Skip Bck Md)] [HH.label_ [HH.text "<<"]]
+              , HH.button [HE.onClick $ HE.input_ (Skip Bck Sm)] [HH.label_ [HH.text "<"]]
+              , HH.button [HE.onClick $ HE.input_ (Skip Fwd Sm)] [HH.label_ [HH.text ">"]]
+              , HH.button [HE.onClick $ HE.input_ (Skip Fwd Md)] [HH.label_ [HH.text ">>"]]
+              , HH.button [HE.onClick $ HE.input_ (Skip Fwd Lg)] [HH.label_ [HH.text ">>>"]]
               ]
           ]
       ]
 
-    eval :: Query ~> H.ComponentDSL State Query Void (AppEffects eff)
+    log' = H.liftAff <<< log
+
+    eval :: Query ~> H.ComponentDSL State Query Void (Aff (AppEffects eff))
     eval (FileSet next) = do
       input <- H.getHTMLElementRef $ wrap "input"
-      case input >>= fromHTMLElement of
-        Nothing -> H.liftAff $ log "No input ref found"
+      case fromHTMLElement =<< input of
+        Nothing -> log' "No input ref found"
         Just el -> handleInput el
       pure next
       where
         handleInput el = do
-          nxs <- H.liftEff <<< files $ el
-          case nxs >>= item 0 of
-            Nothing -> H.liftAff $ log "No file found"
+          nxs <- H.liftEff $ files el
+          case item 0 =<< nxs of
+            Nothing -> log' "No file found"
             Just file -> handleFile file
         handleFile file = do
           url <- H.liftEff $ url =<< window
           blob <- H.liftEff $ createObjectURL file url
           prevBlob <- H.gets _.file
           case prevBlob of
-            Just x ->
-              H.liftEff $ revokeObjectURL (unwrap x) url
+            Just x -> H.liftEff $ revokeObjectURL (unwrap x) url
             _ -> pure unit
           H.modify \s ->
-            s {file = Just <<< wrap $ blob}
+            s {file = pure $ wrap blob}
 
     eval (Skip dir size next) = do
       audio <- H.getHTMLElementRef $ wrap "audio"
-      case audio >>= fromHTMLElement of
+      case htmlAudioElementToHTMLMediaElement <$> (fromHTMLElement =<< audio) of
         Just el -> do
-          let el' = htmlAudioElementToHTMLMediaElement el
-          current <- H.liftEff $ currentTime el'
-          H.liftEff $ setCurrentTime (current + delta) el'
-        _ -> H.liftAff $ log "No audio ref found"
+          current <- H.liftEff $ currentTime el
+          H.liftEff $ setCurrentTime (current + delta) el
+        _ -> log' "No audio ref found"
       pure next
       where
         skip = case size of
@@ -139,12 +138,12 @@ ui =
 
 main :: forall e.
   Eff
-    ( avar :: AVAR
-    , ref :: REF
-    , exception :: EXCEPTION
-    , dom :: DOM
-    , console :: CONSOLE
-    | e
+    (AppEffects
+      ( avar :: AVAR
+      , ref :: REF
+      , exception :: EXCEPTION
+      | e
+      )
     )
     Unit
 main = HA.runHalogenAff do
