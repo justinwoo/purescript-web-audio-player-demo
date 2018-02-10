6a47c3df1e9f40a7d09d2e565fd249dbee508b4c
justinwoo <moomoowoo@gmail.com>
2018-02-10 15:39:24 +0200

    update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-self.addEventListener('install', function(e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open('halogen-web-audio-player-demo').then(function(cache) {
+    caches.open("halogen-web-audio-player-demo").then(function(cache) {
       return cache.addAll([
-        './index.html',
-        './index.html?homescreen=1',
-        './dist/app.js',
-        './dist/app.css'
+        "./index.html",
+        "./index.html?homescreen=1",
+        "./dist/app.js",
+        "./dist/app.css"
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   console.log(event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  fetch(event.request).catch(function() {
+    return caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        alert("File Missing and cannot fetch while offline");
+        throw new Error("File Missing and cannot fetch while offline");
+      }
+    });
+  });
 });
